9f0cda6bd0e8508b2fc6cb763eb6737b11079d26
crandonriordan <crandonriordan@gmail.com>
2022-06-13 15:29:06 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    var name, city, state = "Crandon", "La Grange", "Illinois"
+    fmt.Println("My name is " + name + ". I'm living in " + city + ", " + state + ".");
+}
